151f5ceca240b141666e8319b84efc4910aeb5e7
acareaga <aaroncareaga@gmail.com>
2016-01-18 23:55:15 -0700

    More practice with regex

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 'use strict';
 
-
 var str = `Cat
 sat on
 the hat.`;
 var regex = /\./g
 
-/**
- * @param  String str
- * @param  RegExp regex
- * @param  HTMLElement target
- */
-const output = (str, regex, target) => {
+ // * @param  String str
+ // * @param  RegExp regex
+ @param  HTMLElement target
+
+ const output = (str, regex, target) => {
   target.innerHTML =
     str.replace(regex, str =>
       `<span>${str}</span>`
